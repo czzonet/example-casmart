4bc548ad2c988eeaa565bed0f2aad7abd8853f52
czzonet <czzonet@163.com>
2022-10-03 11:21:51 +0800

    fix: :art: add hot hmr. 添加接收热更新输出

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,9 @@ import "./css/global.css";
 import "./css/reset.css";
 
 ReactDom.render(<App></App>, document.getElementById("app"));
+
+// 接收热更新输出，只有accept才能被更新
+const i: any = module;
+if (i.hot) {
+  i.hot.accept();
+}
